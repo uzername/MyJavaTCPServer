00b076eda72dabb86c74daa1ef867ad07fadbdbe
Николай <@ASCOM_NICHOLAS>
2017-03-27 17:54:22 +0300

    Initial Network code

--- src/myserverlistener/NetworkCode.java
+++ src/myserverlistener/NetworkCode.java
@@ -1,10 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package myserverlistener;
+/**
+ * This code is a java listener on TCP connection (actually a wrapper)
+ * see https://docs.oracle.com/javase/tutorial/networking/sockets/readingWriting.html
+ * see (might be useful too) https://docs.oracle.com/javase/tutorial/networking/sockets/examples/EchoClient.java 
+ * see (that's the main part of code) https://docs.oracle.com/javase/tutorial/networking/sockets/examples/EchoServer.java
+ * @author Ivan
+ */
+
+import java.net.*;
+import java.io.*;
 
 public class NetworkCode {
+    private Integer portNumber;
+
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    //read data from socket
+    private BufferedReader in;
+    /**
+     * @return the portNumber
+     */
+    public Integer getPortNumber() {
+        return portNumber;
+    }
+
+    /**
+     * @param portNumber the portNumber to set
+     */
+    public void setPortNumber(Integer portNumber) {
+        this.portNumber = portNumber;
+    }
     
 }
